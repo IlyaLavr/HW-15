0546e53341056a07bb0daf38c789f3e82d6006bf
Ilya <iilavr@mail.ru>
2022-10-11 22:49:02 +0300

    add-table-view

--- HW-15/Sources/Scenes/Controller/ViewController.swift
+++ HW-15/Sources/Scenes/Controller/ViewController.swift
@@ -5,15 +5,24 @@
 //  Created by Илья on 11.10.2022.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var model: OnboardingModel?
+    var models: [Sections] = OnboardingModel.getSections()
+    private var View: OnboardingView? {
+        guard isViewLoaded else { return nil }
+        return view as? OnboardingView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = OnboardingView(viewControl: self)
+        model = OnboardingModel()
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
 
